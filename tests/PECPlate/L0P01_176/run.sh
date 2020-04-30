e54380dde46e6bb5b1abde4addcc9367870303b2
Jun Wang <junwang9@levlabserver2.stanford.edu>
2020-04-30 16:27:55 -0700

    Two more Plate CasPol test

---
+++ tests/PECPlate/L0P01_176/run.sh
@@ -0,0 +1,1 @@
+scuff-caspol --geometry AlPlate_176.scuffgeo --EPFile ../EPFile --Atom Rb --XiFile ../XiFile --BZIMethod TC --BZIOrder 9 --BZSymmetryFactor 8
---
+++ tests/PECPlate/L0P01_40/run.sh
@@ -0,0 +1,1 @@
+scuff-caspol --geometry PECPlate_L0P01_40.scuffgeo --EPFile ../EPFile --Atom Rb --XiFile ../XiFile --BZIMethod TC --BZIOrder 9 --BZSymmetryFactor 8
