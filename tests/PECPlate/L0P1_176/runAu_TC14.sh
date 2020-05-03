66c0adb46b7541ac5389fe280db38c6d77ead634
congzlwag <congzlwag@163.com>
2020-05-02 20:18:59 -0700

    L0.1_176 convergence supp test

---
+++ tests/PECPlate/L0P1_176/runAu_TC14.sh
@@ -0,0 +1,1 @@
+scuff-caspol --geometry AuPlate_176.scuffgeo --EPFile ../EPFile --Atom Rb --XiFile ../XiFile --BZIMethod TC  --BZIOrder 14 --BZSymmetryFactor 8
---
+++ tests/PECPlate/L0P1_176/runAu_TC20.sh
@@ -0,0 +1,1 @@
+scuff-caspol --geometry AuPlate_176.scuffgeo --EPFile ../EPFile --Atom Rb --XiFile ../XiFile --BZIMethod TC  --BZIOrder 20 --BZSymmetryFactor 8
